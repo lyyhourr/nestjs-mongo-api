55a90ef4e12c05bead4c2024061ffdca826bf375
lyhour <153416990+lyyhourr@users.noreply.github.com>
2024-06-24 16:11:33 +0700

    Delete dist directory

--- dist/auth/constant/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.IS_PUBLIC_KEY = exports.jwtConstants = void 0;
-exports.jwtConstants = {
-    secret: 'abcdefghijklmnopqrstuvwxyz',
-};
-exports.IS_PUBLIC_KEY = 'isPublic';
-//# sourceMappingURL=constants.js.map
